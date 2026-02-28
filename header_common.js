eb9dca244247138303bf61c930f035dcbea4e4fd
UltimaN3rd <contact@croakingkero.com>
2026-02-27 21:04:46 -0600

    Replace blog with Golden Path

--- header_common.js
+++ header_common.js
@@ -1,6 +1,6 @@
 document.currentScript.insertAdjacentHTML('beforebegin',`
 <header style="text-align: center; display: block;">
 <img src="/images/croakingkero.png" alt="Croaking Kero" style="width:192px;height:168px;"/>
-<br/><nav><h4><a href="/">Games</a> | <a href="/blog">Blog</a></h4></nav>
+<br/><nav><h4><a href="/">Games</a> | <a href="https://goldenpathprogramming.com/">Learn to Code Games</a></h4></nav>
 </header>
 `);
